9ebb14ed8c144d7c2a1c1717c5f279e64f2c8b56
Joseph Lin <joseph.lin.163@gmail.com>
2015-05-14 22:35:29 -0700

    Added wordlist and updated code to output both unmatched and matched.

--- alphabetIsolater.cpp
+++ alphabetIsolater.cpp
@@ -7,6 +7,7 @@ int main() {
 
     // setting up the list of alphabet pairs
     vector<string> pairs;
+    vector<string> pairs2;
     string a,b;
     for (int i = 0; i < 26; i++) {
         a = 97+i;
@@ -33,11 +34,47 @@ int main() {
                 }
             }
         }
-        // cout << pairs.size() << endl;
+         //cout << pairs.size() << endl;
 	}
+    
+    cout << "List of alphabet pairs that do not show up:\n";
     
     // cout list of alphabet pairs that do not exist in the alphabet
     for (int i = 0; i < pairs.size(); i++) {
-        cout << pairs[i] << endl;
+        cout << pairs[i] << " ";
     }
+    
+    cout << "\n";
+    
+    // setting up pairs2 to get list of alphabet pairs that do exist => "match"
+    for (int i = 0; i < 26; i++) {
+        a = 97+i;
+        for (int j = 0; j < 26; j++) {
+            b = 97 + j;
+            string temp = a+b;
+            pairs2.push_back(temp);
+        }
+    }
+    
+    // remove the unmatched pairs from the original list of 676 alphabetical pairs
+    for (int i = 0; i < pairs.size(); i++) {
+        for (int j = 0; j < pairs2.size(); j++) {
+            if (pairs[i] == pairs2[j]) {
+                pairs2.erase(pairs2.begin() + j);
+                i--;
+                break;
+            }
+        }
+    }
+    
+    cout << "List of alphabet pairs that do show up:\n";
+    
+    // cout list of alphabet pairs that do exist in the alphabet
+    for (int i = 0; i < pairs2.size(); i++) {
+        cout << pairs2[i] << " ";
+    }
+    
+    cout << "\n";
+    
+    return 0;
 }
